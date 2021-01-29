6eff85793002a308d8ea66b4bb6e79176938a17d
unknown <you@example.com>
2021-01-28 20:06:24 -0500

    Second commit

---
+++ Unity.js
@@ -0,0 +1,370 @@
+function displayInfo() {
+        var country = [];
+        var currency = [];
+        var capital = [];
+        var language = [];
+        var minister = [];
+        
+        
+        country[0] = "Angola";
+        currency[0] = "Kwanza";
+        capital[0] = "Luanda";
+        language[0] = "portugese";
+        minister[0] = "João Lourenço";
+        
+         country[1] = "Australia";
+        currency[1] = "dollar";
+        capital[1] = "Canberra";
+        language[1] = "English";
+        minister[1] = "Scott Morrison";
+        
+         country[2] = "Andorra";
+        currency[2] = "Euro";
+        capital[2] = "Andorra la Vella";
+        language[2] = "Catalan";
+        minister[2] = "Xavier Espot Zamora";
+        
+         country[3] = "Argentina";
+        currency[3] = "Peso";
+        capital[3] = "Buenos Aires";
+        language[3] = "Espanol";
+        minister[3] = "Alberto Fernandez";
+        
+         country[4] = "Bahrain";
+        currency[4] = "dinar";
+        capital[4] = "Manama";
+        language[4] = "Arabic";
+        minister[4] = "Khalifa bin Salman Al Khalifa";
+        
+         country[5] = "Belize";
+        currency[5] = "dollar";
+        capital[5] = "Belmopan";
+        language[5] = "English Belizean Creole, Garifuna, Mayan languages, Spanish";
+        minister[5] = "Juan Briceno";
+        
+         country[6] = "Brazil";
+        currency[6] = "Real";
+        capital[6] = "Brasila";
+        language[6] = "Portuguese";
+        minister[6] = "Jair Bolsonaro";
+        
+         country[7] = "Chile";
+        currency[7] = "Peso";
+        capital[7] = "Santiago de Chile";
+        language[7] = "Espanol";
+        minister[7] = "Sebastián Piñera";
+        
+         country[8] = "Cape Verde";
+        currency[8] = "escudo";
+        capital[8] = "Pria";
+        language[8] = "portugese";
+        minister[8] = "Jorge Carlos Fonseca";
+        
+        country[9] = "China";
+        currency[9] = "Yuan";
+        capital[9] = "Beijing";
+        language[9] = "Mandarin";
+        minister[9] = "Xi Jinping";
+        
+        country[10] = "Canada";
+        currency[10] = "Dollar";
+        capital[10] = "Ottawa";
+        language[10] = "English, French";
+        minister[10] = "Justin Trudeau";
+        
+        country[11] = "China";
+        currency[11] = "Yuan";
+        capital[11] = "Beijing";
+        language[11] = "Mandarin";
+        minister[11] = "Xi Jinping";
+        
+        country[12] = "Denmark";
+        currency[12] = "Euro";
+        capital[12] = "Copenhagen";
+        language[12] = "Danish,German";
+        minister[12] = "Mette Frederiksen";
+
+        country[13] = "Dominican Republic";
+        currency[13] = "Peso";
+        capital[13] = "Santo Domingo";
+        language[13] = "Spanish";
+        minister[13] = "Luis Rodolfo Abinader Corona";
+        
+         country[14] = "Estonia";
+        currency[14] = "Euro";
+        capital[14] = "Tallinn";
+        language[14] = "Estonian";
+        minister[14] = "Kersti Kaljulaid";
+        
+        country[15] = "Egypt";
+        currency[15] = "Pound";
+        capital[15] = "Cairo";
+        language[15] = "Arabic";
+        minister[15] = "Abdel Fattah el-Sisi";
+        
+        country[16] = "El Salvador";
+        currency[16] = "Dollar";
+        capital[16] = "San Salvador";
+        language[16] = "Spanish";
+        minister[16] = "Nayib Bukele";
+        
+        country[17] = "France";
+        currency[17] = "Euro";
+        capital[17] = "Paris";
+        language[17] = "French";
+        minister[17] = "Emmanuel Macron";
+        
+        country[18] = "Finland";
+        currency[18] = "Euro";
+        capital[18] = "Helsinki";
+        language[18] = "Swedish";
+        minister[18] = "Sauli Niinistö";
+        
+        country[19] = "Fiji";
+        currency[19] = "Dollar";
+        capital[19] = "Suava";
+        language[19] ="Bau";
+        minister[19] = "Jioji Konrote";
+        
+         country[20] = "Gambia";
+        currency[20] = "Dalasi";
+        capital[20] = "Banjul";
+        language[20] = "English";
+        minister[20] = "Adama Barrow";
+        
+        country[21] = "Gabon";
+        currency[21] = "Franc";
+        capital[21] = "Libreville";
+        language[21] = "French";
+        minister[21] = "Ali Bongo Ondimba";
+        
+        country[22] = "Germany";
+        currency[22] = "Euro";
+        capital[22] = "Munich";
+        language[22] = "Euro";
+        minister[22] = "Frank-Walter Steinmeier";
+        
+        country[23] = "Guatemala";
+        currency[23] = "Quetzal";
+        capital[23] = "Guatemala City";
+        language[23] = "Spanish";
+        minister[23] = "Alejandro Giammattei";
+      
+        country[24] = "Hungria";
+        currency[24] = "Forint";
+        capital[24] = "Budapest";
+        language[24] = "Hungarian";
+        minister[24] = "Viktor Orbán";
+        
+        country[25] = "Honduras";
+        currency[25] = "Lempira";
+        capital[25] = "Tegucigalpa";
+        language[25] = "Spanish, Guarifuana";
+        minister[25] = "Juan Hernandez";
+        
+        country[26] = "Iceland";
+        currency[26] = "Krona";
+        capital[26] = "Reykjavík";
+        language[26] = "Icelandic";
+        minister[26] = "Katrín Jakobsdóttir";
+        
+        country[27] = "India";
+        currency[27] = "Rupee";
+        capital[27] = "New Dehli";
+        language[27] = "Hindi";
+        minister[27] = "Ram Nath Kovind";
+        
+        country[28] = "Jordan";
+        currency[28] = "dinar";
+        capital[28] = "Amman";
+        language[28] = "Arabic";
+        minister[28] = "Bisher Al-Khasawneh";
+        
+        country[29] = "Japan";
+        currency[29] = "Yen";
+        capital[29] = "Tokyo";
+        language[29] = "Japanese";
+        minister[29] = "Yoshihide Suga";
+        
+        country[30] = "Jamaica";
+        currency[30] = "Dollar";
+        capital[30] = "Kingston";
+        language[30] = "English";
+        minister[30] = "Andrew Holness";
+        
+        country[31] = "Kenya";
+        currency[31] = "Shilling";
+        capital[31] = "Nairobi";
+        language[31] = "English,Swahili";
+        minister[31] = "Uhuru Muigai Kenyatta";
+        
+        country[32] = "Kuwait";
+        currency[32] = "Dollar";
+        capital[32] = "Kuwait City";
+        language[32] = "Arabic";
+        minister[32] = "Sabah Al-Khalid Al-Sabah";
+        
+        country[33] = "Lithuania";
+        currency[33] = "Euro";
+        capital[33] = "Vilnius";
+        language[33] = "Lithuanian";
+        minister[33] = "Gitanas Nausėda";
+        
+        country[34] = "Libya";
+        currency[34] = "Dinar";
+        capital[34] = "Tripoli";
+        language[34] = "Arabic";
+        minister[34] = "Fayez al-Sarraj";
+        
+        country[35] = "Morroco";
+        currency[35] = "dirham";
+        capital[35] = "Rabat";
+        language[35] = "Arabic,Berber";
+        minister[35] = "Saadeddine Othmani";
+        
+        country[36] = "Macedonia";
+        currency[36] = "Denar";
+        capital[36] = "Skopje";
+        language[36] = "Macedonian";
+        minister[36] = "Gjorge Ivanov";
+        
+        country[37] = "Nigeria";
+        currency[37] = "Niara";
+        capital[37] = "Lagos";
+        language[37] = "Igbo,English";
+        minister[37] = "Muhammadu Buhari";
+        
+        country[38] = "North Korea";
+        currency[38] = "Won";
+        capital[38] = "Pyongyang";
+        language[38] = "Korean";
+        minister[38] = "Kim Yun Un";
+        
+        country[39] = "Oman";
+        currency[39] = "Rial ";
+        capital[39] = "Muscat";
+        language[39] = "Arabic";
+        minister[39] = "Haitham bin Tariq";
+        
+        country[40] = "Panama";
+        currency[40] = "Balboa ";
+        capital[40] = "Panama City";
+        language[40] = "Spanish";
+        minister[40] = "Laurentino Cortizo";
+        
+         country[41] = "Pakistan";
+        currency[41] = "Rupee";
+        capital[41] = "Islamabad";
+        language[41] = "English,Urdu";
+        minister[41] = "Arif Alvi";
+        
+        country[42] = "Qatar";
+        currency[42] = "Riyal";
+        capital[42] = "Doha";
+        language[42] = "English";
+        minister[42] = "Sheikh Khalid bin Khalifa bin Abdul Aziz Al Thani";
+        
+        country[43] = "Russia";
+        currency[43] = "Ruble";
+        capital[43] = "Moscow";
+        language[43] = "Russian";
+        minister[43] = "Vlademir Putin";
+        
+         country[44] = "Romania";
+        currency[44] = "Rupee";
+        capital[44] = "Bucharest";
+        language[44] = "Romanian";
+        minister[44] = "Florin Cîțu";
+        
+        country[45] = "South Africa";
+        currency[45] = "Rand";
+        capital[45] = "Pretoria";
+        language[45] = "Zulu";
+        minister[45] = "Cyril Ramaphosa";
+        
+         country[46] = "Spain";
+        currency[46] = "Euro";
+        capital[46] = "Madrid";
+        language[46] = "Spanish";
+        minister[46] = "Pedro Sánchez";
+        
+        country[47] = "Thailand";
+        currency[47] = "Baht";
+        capital[47] = "Bangkok";
+        language[47] = "Thai";
+        minister[47] = "Prayut Chan-o-cha";
+        
+        country[48] = "Tanzania";
+        currency[48] = "Schilling";
+        capital[48] = "Dodama";
+        language[48] = "Swahili";
+        minister[48] = "John Magufuli";
+        
+        country[49] = "United Arab Emirates";
+        currency[49] = "Dirham";
+        capital[49] = "Abu Dhabi";
+        language[49] = "Arabic";
+        minister[49] = "Khalifa bin Zayed Al Nahyan";
+        
+        country[50] = "United States";
+        currency[50] = "Dollar";
+        capital[50] = "Washington D.C";
+        language[50] = "English,Spanish";
+        minister[50] = "Donald Trump";
+        
+        country[51] = "Venezuela";
+        currency[51] = "Bolivar";
+        capital[51] = "Caracas";
+        language[51] = "Spanish";
+        minister[51] = "Nicolas Maduro";
+        
+        country[52] = "Vietnam";
+        currency[52] = "Dong";
+        capital[52] = "Hanoi";
+        language[52] = "Vietnamese";
+        minister[52] = "Nguyen Phu Trong";
+        
+        country[53] = "Yemen";
+        currency[53] = "Rial";
+        capital[53] = "	Sanaa";
+        language[53] = "Arabic";
+        minister[53] = "Abdrabbuh Mansur Hadi";
+        
+        country[54] = "Zambia";
+        currency[54] = "Kwacha";
+        capital[54] = "Lusaka";
+        language[54] = "English";
+        minister[54] = "Edgar Lungu";
+        
+        country[55] = "Zimbabwe";
+        currency[55] = "Dollar";
+        capital[55] = "Harare";
+        language[55] = "English";
+        minister[55] = "Emmerson Mnangagwa";
+        
+        
+        
+        
+        
+        var choice = document.getElementById("countries").value;
+        
+        for(var i =0;i<country.length;i++) {
+          if(country[i] == choice) {
+            document.getElementById("current").value = capital[i];
+            document.getElementById("money").value = currency[i];
+            document.getElementById("lengua").value = language[i];
+            document.getElementById("lider").value = minister[i];
+            break;
+          }
+          else 
+          {
+            document.getElementById("current").value ="";
+            document.getElementById("money").value ="";
+            document.getElementById("lengua").value ="";
+            document.getElementById("lider").value ="";
+          }
+        }
+        
+        
+        
+  }
